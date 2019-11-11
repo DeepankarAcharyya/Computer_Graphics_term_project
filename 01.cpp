86046ff2b2a555705e97d8e7501b7fd8c0819a06
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-11-11 20:51:26 +0530

    the basics

---
+++ 01.cpp
@@ -0,0 +1,58 @@
+#include <GL/glut.h>
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);   // Clear the color buffer with current clearing color
+ 
+   // Define shapes enclosed within a pair of glBegin and glEnd
+   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+      glColor3f(1.0f, 0.0f, 0.0f); // Red
+      glVertex2f(-0.8f, 0.1f);     // Define vertices in counter-clockwise (CCW) order
+      glVertex2f(-0.2f, 0.1f);     
+      glVertex2f(-0.2f, 0.7f);
+      glVertex2f(-0.8f, 0.7f);
+ 
+      glColor3f(0.0f, 1.0f, 0.0f); // Green
+      glVertex2f(-0.7f, -0.6f);
+      glVertex2f(-0.1f, -0.6f);
+      glVertex2f(-0.1f,  0.0f);
+      glVertex2f(-0.7f,  0.0f);
+ 
+   glEnd();
+ 
+   glBegin(GL_TRIANGLES);          // Each set of 3 vertices form a triangle
+      glColor3f(0.0f, 0.0f, 1.0f); // Blue
+      glVertex2f(0.1f, -0.6f);
+      glVertex2f(0.7f, -0.6f);
+      glVertex2f(0.4f, -0.1f);
+ 
+   glEnd();
+ 
+   glBegin(GL_POLYGON);            // These vertices form a closed polygon
+      glColor3f(1.0f, 1.0f, 0.0f); // Yellow
+      glVertex2f(0.4f, 0.2f);
+      glVertex2f(0.6f, 0.2f);
+      glVertex2f(0.7f, 0.4f);
+      glVertex2f(0.6f, 0.6f);
+      glVertex2f(0.4f, 0.6f);
+      glVertex2f(0.3f, 0.4f);
+   glEnd();
+ 
+   glFlush();  // Render now
+}
+
+int main(int argc,char** argv){
+    //Initialize glut
+    glutInit(&argc,argv);
+    //creating a window
+    glutCreateWindow("Program 001");
+    //Set the window's initial width and height
+    glutInitWindowSize(320,320);
+    //Initial position of the top-left corner
+    glutInitWindowPosition(50,50);
+    //display call back function
+    glutDisplayFunc(display);
+    //enter the event-processing loop
+    glutMainLoop();
+    
+    return 0;
+}
